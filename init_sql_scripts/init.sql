8db65354f2354cc819a5b8baa9239772ea79342a
GayanKavinda <gayan982822513.lk@gmail.com>
2022-11-11 20:25:42 +0530

    Cloud Computing Assignment

---
+++ init_sql_scripts/init.sql
@@ -0,0 +1,9 @@
+create table name_and_email (
+	stdname text,
+	email text
+);
+
+insert into name_and_email(stdname, email) values ('shan', 'abc@gmail');
+insert into name_and_email(stdname, email) values ('santhush', 'ghf@gmail.com');
+insert into name_and_email(stdname, email) values ('amali', 'jhg@gmail.com');
+insert into name_and_email(stdname, email) values ('nethuli', 'bfv@gmail.com');
